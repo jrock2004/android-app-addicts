35e7987c21aabdd2f21a95f0ca14dd14506715bf
John Costanzo <jrock2004@gmail.com>
2021-12-24 10:45:12 -0500

    chore: working on app lit

--- src/components/PreviewAppList/index.tsx
+++ src/components/PreviewAppList/index.tsx
@@ -1,9 +1,34 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { PreviewApp } from '../PreviewApp';
 
+export type AppType = {
+  show: string;
+  name: string;
+  url: string;
+  provider: string;
+  price: string;
+  imageUrl: string;
+  vote: string;
+};
+
 export const PreviewAppList: React.FC = (): ReactElement => {
+  const [apps, setApps] = useState<AppType[]>([]);
+
+  useEffect(() => {
+    async () => {
+      const response = await fetch('http://localhost:3005/api/apps');
+      const json: AppType[] = response.json();
+
+      if (json) {
+        setApps(json);
+      }
+    };
+  }, []);
+
+  console.log(apps);
+
   return (
     <section className="mb-12 p-4">
       <div className="flex items-baseline justify-between mb-4">
